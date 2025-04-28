c50b9637948c87637a255a6ce675a40007e115aa
robin-hassan <robiul.hassan12102@gmail.com>
2025-04-28 03:11:56 -0400

    finish em

---
+++ app/controllers/employees_controller.rb
@@ -0,0 +1,15 @@
+class EmployeesController < ApplicationController
+  def index
+    organization_memberships = Clerk::SDK.new.organization_memberships
+    list_organization_memberships = organization_memberships.list_organization_memberships('org_2vLMkZCAXOv6VCUPj24tb6Md5OV', list_organization_memberships_request: {limit: 10, offset: 0, role: [ 'org:carbon_credits_employee' ]})    
+    render json: list_organization_memberships, status: :ok
+  end
+  
+
+  def create
+    organization_invitations = Clerk::SDK.new.organization_invitations
+    create_invitation = organization_invitations.create_organization_invitation('org_2vLMkZCAXOv6VCUPj24tb6Md5OV', create_organization_invitation_request: {role: 'org:carbon_credits_employee',email_address: params[:email_address]})
+    # puts "User created: #{organization_invitations}"    
+    render json: { message: "Employer invitation sent successfully", invitation: create_invitation }, status: :created
+  end
+end
